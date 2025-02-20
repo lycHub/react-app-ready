29f79e008d218fa24c7f0a1affe1e1dc62201d01
ysx <1120805364@qq.com>
2025-02-20 09:29:32 +0800

    feat(wechat): 小程序icon

---
+++ apps/wechat/icon.js
@@ -0,0 +1,59 @@
+import { writeFileSync } from "node:fs";
+import { getIconCSS } from "@iconify/utils";
+import {
+  importDirectory,
+  cleanupSVG,
+  runSVGO,
+  parseColors,
+  isEmptyColor,
+} from "@iconify/tools";
+
+const svgPath = "miniprogram/public/svgs";
+const destPath = "miniprogram/icon.scss";
+const ignoreNames = ["logo"];
+
+(async () => {
+  const iconSet = await importDirectory(svgPath, {
+    prefix: "zs",
+    ignoreImportErrors: false,
+  });
+
+  let cssStr = "";
+  iconSet.forEach((name, type) => {
+    if (type !== "icon" || ignoreNames.includes(name)) return;
+
+    const svg = iconSet.toSVG(name);
+    if (!svg) {
+      // Invalid icon
+      iconSet.remove(name);
+      return;
+    }
+
+    try {
+      cleanupSVG(svg);
+      parseColors(svg, {
+        defaultColor: "currentColor",
+        callback: (attr, colorStr, color) => {
+          return !color || isEmptyColor(color) ? colorStr : "currentColor";
+        },
+      });
+
+      // Optimise
+      runSVGO(svg);
+    } catch (err) {
+      // Invalid icon
+      console.error(`ICON Error parsing ${name}:`, err);
+      iconSet.remove(name);
+      return;
+    }
+
+    const iconData = svg.getIcon();
+    cssStr +=
+      getIconCSS(iconData, {
+        iconSelector: ".zs-icon__" + name,
+      }) + "\n";
+  });
+
+  writeFileSync(destPath, cssStr, "utf8");
+  // todo: watch svgPath by chokidar
+})();
